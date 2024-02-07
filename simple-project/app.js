3b664b4929d992f026216e6652e53a3f62f49726
marwan-pixel <marwanhadid6@gmail.com>
2024-02-07 10:44:16 +0700

    Belajar Vue.JS Element Binding

---
+++ simple-project/app.js
@@ -0,0 +1,11 @@
+var data = {
+    name: "Xiaomi 13 Ultra",
+    description: "Xiaomi Flagship Smartphone",
+    price: '£849',
+    image: 'https://www.androidauthority.com/wp-content/uploads/2023/04/Xiaomi-13-Ultra-white-model-resize-840w-472h.jpg.webp'
+};
+
+var app = new Vue({
+    el: '#app',
+    data: data
+});
