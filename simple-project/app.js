ebe56dcd50d10c041798bd91b000b4a43fc66d8c
marwan-pixel <marwanhadid6@gmail.com>
2024-02-07 11:34:42 +0700

    Belajar Vue.JS Fungsi Kondisi di dalam VueJS

--- simple-project/app.js
+++ simple-project/app.js
@@ -1,27 +1,28 @@
 var dataProduct = {
+    maximum: 500,
     products: [
         {
             name: "Xiaomi 13 Ultra",
             description: "Xiaomi Ultra Flagship Smartphone",
-            price: '$849.50',
+            price: '849.50',
             image: 'https://www.androidauthority.com/wp-content/uploads/2023/04/Xiaomi-13-Ultra-white-model-resize-840w-472h.jpg.webp'
         },
         {
             name: "Xiaomi 13 Pro",
             description: "Xiaomi Flagship Smartphone",
-            price: '$1,149.99',
+            price: '1149.99',
             image: 'https://cdn.dxomark.com/wp-content/uploads/medias/post-139229/Xiaomi-13-Pro_featured-image-packshot-review-Recovered.jpg'
         },
         {
             name: "Xiaomi 13",
             description: "Xiaomi Compact Flagship Smartphone",
-            price: '$663.14',
+            price: '663.14',
             image: 'https://www.phoneworld.com.pk/wp-content/uploads/2023/04/xiaomi-13T-Pro-Specs.jpg'
         },
         {
             name: "Xiaomi 13 Lite",
             description: "Xiaomi Midrange Smartphone",
-            price: '$324.99',
+            price: '324.99',
             image: 'https://gizchina.it/wp-content/uploads/2022/10/Xiaomi-13-lite-civi-2.jpg'
         },
 
