a7f305f4c9725f10500fd250d7d453c604bd78d3
marwan-pixel <marwanhadid6@gmail.com>
2024-02-07 14:13:00 +0700

    Event dan Method pada VueJS

--- simple-project/app.js
+++ simple-project/app.js
@@ -32,7 +32,8 @@ var app = new Vue({
     el: '#app',
     data: {
         maximum: 50,
-        products: null
+        products: null,
+        cart: []
     },
     mounted: function() {
         fetch('https://hplussport.com/api/products/order/price')
@@ -40,5 +41,10 @@ var app = new Vue({
         .then(data => {
             this.products = data;
         })
+    },
+    methods: {
+        addItem: function (product) {
+            this.cart.push(product);
+        }
     }
 });
