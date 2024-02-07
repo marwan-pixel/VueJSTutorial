03c90e053b750913a5660c0a4644c901df418011
marwan-pixel <marwanhadid6@gmail.com>
2024-02-07 14:24:49 +0700

    Interpolation Template

--- simple-project/app.js
+++ simple-project/app.js
@@ -33,7 +33,8 @@ var app = new Vue({
     data: {
         maximum: 50,
         products: null,
-        cart: []
+        cart: [],
+        name: 'Hello World'
     },
     mounted: function() {
         fetch('https://hplussport.com/api/products/order/price')
