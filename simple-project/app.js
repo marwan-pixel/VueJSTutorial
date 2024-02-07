d29892c8072eee87eda0db99fb5178ffcb1517ae
marwan-pixel <marwanhadid6@gmail.com>
2024-02-07 11:26:42 +0700

    Belajar Vue.js Membuat Looping di dalam Vue.js

--- simple-project/app.js
+++ simple-project/app.js
@@ -1,11 +1,34 @@
-var data = {
-    name: "Xiaomi 13 Ultra",
-    description: "Xiaomi Flagship Smartphone",
-    price: '£849',
-    image: 'https://www.androidauthority.com/wp-content/uploads/2023/04/Xiaomi-13-Ultra-white-model-resize-840w-472h.jpg.webp'
+var dataProduct = {
+    products: [
+        {
+            name: "Xiaomi 13 Ultra",
+            description: "Xiaomi Ultra Flagship Smartphone",
+            price: '$849.50',
+            image: 'https://www.androidauthority.com/wp-content/uploads/2023/04/Xiaomi-13-Ultra-white-model-resize-840w-472h.jpg.webp'
+        },
+        {
+            name: "Xiaomi 13 Pro",
+            description: "Xiaomi Flagship Smartphone",
+            price: '$1,149.99',
+            image: 'https://cdn.dxomark.com/wp-content/uploads/medias/post-139229/Xiaomi-13-Pro_featured-image-packshot-review-Recovered.jpg'
+        },
+        {
+            name: "Xiaomi 13",
+            description: "Xiaomi Compact Flagship Smartphone",
+            price: '$663.14',
+            image: 'https://www.phoneworld.com.pk/wp-content/uploads/2023/04/xiaomi-13T-Pro-Specs.jpg'
+        },
+        {
+            name: "Xiaomi 13 Lite",
+            description: "Xiaomi Midrange Smartphone",
+            price: '$324.99',
+            image: 'https://gizchina.it/wp-content/uploads/2022/10/Xiaomi-13-lite-civi-2.jpg'
+        },
+
+    ]
 };
 
 var app = new Vue({
     el: '#app',
-    data: data
+    data: dataProduct
 });
