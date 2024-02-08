bf1e33c41bfe320417d21991573859bf9426bb7b
marwan-pixel <marwanhadid6@gmail.com>
2024-02-08 19:35:31 +0700

    Membuat Component yang lebih kompleks

--- simple-project/app.js
+++ simple-project/app.js
@@ -29,21 +29,72 @@
 // };
 
 Vue.component('price', {
-    data: function() {
+    data: function () {
         return {}
     },
     props: {
-        'value': Number, 
+        'value': Number,
         'prefix': {
             type: String,
             default: 'Rp'
         },
         precision: {
             type: Number,
-            default:2
+            default: 2
         }
     },
     template: `<span>{{ this.prefix + Number.parseFloat(this.value).toFixed(this.precision) }} </span>`
+});
+
+Vue.component('product-list', {
+    props: ['products', 'maximum'],
+    methods: {
+        before: function (el) {
+            el.className = 'd-none';
+        },
+        enter: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function () {
+                el.className = 'row d-flex mb-3 align-items-center animate__animated animate__fadeInRight';
+            }, delay);
+        },
+        leave: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function () {
+                el.className = 'row d-flex mb-3 align-items-center animate__animated animate__fadeOutRight';
+            }, delay);
+        },
+    },
+    template: `
+    <transition-group name="fade" tag="div" @beforeEnter="before" @enter="enter" @leave="leave">
+        <div class="row d-none mb-3 align-items-center" v-for="(item, index) in products" :key="item.id"
+            v-if="item.price <= Number(maximum)" :data-index="index">
+            <div class="col-1 m-auto">
+                <button class="btn btn-info text-white" @click.stop="addItem(item)">+</button>
+            </div>
+            <div class="col-sm-4">
+                <img class="img-fluid d-block" :src="item.image" :alt="item.name">
+                <!-- <img v-bind="{
+                    class: imgClass,
+                    src: item.image,
+                    alt: item.name
+                }"> -->
+            </div>
+            <div class="col">
+                <h3 class="text-dark fw-bold">{{ item.name }}</h3>
+                <p class="mb-0S">{{ item.description }}</p>
+                <div class="h5 float-right">
+                    <price 
+                        :value="Number(item.price)"
+                        :prefix="'$'" 
+                        :precision="2"
+                    ></price>
+                </div>
+            </div>
+        </div>
+
+    </transition-group>
+    `
 });
 
 var app = new Vue({
@@ -61,15 +112,15 @@ var app = new Vue({
         name: 'Hello World',
         slugText: 'The Quick #(*!&@ Brown 29'
     },
-    mounted: function() {
+    mounted: function () {
         fetch('https://hplussport.com/api/products/order/price')
-        .then(response => response.json())
-        .then(data => {
-            this.products = data;
-        })
+            .then(response => response.json())
+            .then(data => {
+                this.products = data;
+            })
     },
     computed: {
-        now: function() {
+        now: function () {
             var date = new Date();
             return (
                 String(date.getHours()) +
@@ -77,23 +128,23 @@ var app = new Vue({
                 String(date.getSeconds())
             );
         },
-        slugetize: function() {
+        slugetize: function () {
             return this.slugText
-                    .toLowerCase()
-                    .replace(/[^\w]+/g, '')
-                    .replace(/ +/g, '-') + '-' + this.now();
+                .toLowerCase()
+                .replace(/[^\w]+/g, '')
+                .replace(/ +/g, '-') + '-' + this.now();
         },
-        sliderState: function() {
+        sliderState: function () {
             return this.style.sliderStatus ? 'd-flex' : 'd-none';
         },
-        cartTotal: function() {
+        cartTotal: function () {
             let sum = 0;
             for (key in this.cart) {
                 sum = sum + (this.cart[key].product.price * this.cart[key].qty);
             }
             return sum;
         },
-        cartQty: function() {
+        cartQty: function () {
             let qty = 0;
             for (key in this.cart) {
                 qty = qty + this.cart[key].qty
@@ -102,30 +153,16 @@ var app = new Vue({
         }
     },
     filters: {
-        currencyFormat: function(value) {
+        currencyFormat: function (value) {
             return `$${Number.parseFloat(value).toFixed(2)}`;
         }
     },
     methods: {
-        before: function(el) {
-            el.className = 'd-none';
-        },
-        enter: function(el) {
-            var delay = el.dataset.index * 100;
-            setTimeout(function() {
-                el.className = 'row d-flex mb-3 align-items-center animate__animated animate__fadeInRight';
-            }, delay);
-        },
-        leave: function(el) {
-            var delay = el.dataset.index * 100;
-            setTimeout(function() {
-                el.className = 'row d-flex mb-3 align-items-center animate__animated animate__fadeOutRight';
-            }, delay);
-        },
+       
         addItem: function (product) {
             var productIndex;
             var productExist = this.cart.filter(function (item, index) {
-                if(item.product.id == Number(product.id)){
+                if (item.product.id == Number(product.id)) {
                     productIndex = index;
                     return true;
                 } else {
@@ -135,11 +172,11 @@ var app = new Vue({
             if (productExist.length) {
                 this.cart[productIndex].qty++;
             } else {
-                this.cart.push({product: product, qty: 1});
+                this.cart.push({ product: product, qty: 1 });
             }
         },
-        deleteItem: function(key) {
-            if(this.cart[key].qty > 1) {
+        deleteItem: function (key) {
+            if (this.cart[key].qty > 1) {
                 this.cart[key].qty--;
             } else {
                 this.cart.splice(key, 1);
